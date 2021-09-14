e01bbc7da8fd99f208bec8233b21f8350ffd67f0
MahdiDev78 <MahdiDev78@gmail.com>
2021-09-14 16:41:23 +0430

    use ViewPagerIndicator module in the app module

--- app/src/main/java/com/mahdikh/viewpagerindicator/MainActivity.java
+++ app/src/main/java/com/mahdikh/viewpagerindicator/MainActivity.java
@@ -1,8 +1,18 @@
 package com.mahdikh.viewpagerindicator;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.RecyclerView;
+import androidx.viewpager2.widget.ViewPager2;
+
+import com.mahdikh.vision.viewpagerindicator.progress.InstanceProgress;
+import com.mahdikh.vision.viewpagerindicator.widget.PagerIndicator;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +20,39 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ViewPager2 viewPager2 = findViewById(R.id.viewPager2);
+        viewPager2.setAdapter(new PagerAdapter());
+
+        PagerIndicator pagerIndicator = findViewById(R.id.pagerIndicator);
+        pagerIndicator.setupWithViewPager2(viewPager2);
+        pagerIndicator.setProgress(new InstanceProgress());
+    }
+
+    public static final class PagerAdapter extends RecyclerView.Adapter<PagerAdapter.ViewHolder> {
+        @NonNull
+        @Override
+        public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+            return new ViewHolder(LayoutInflater.from(parent.getContext()).inflate(R.layout.item, parent, false));
+        }
+
+        @Override
+        public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
+            holder.textView.setText(String.valueOf(position));
+        }
+
+        @Override
+        public int getItemCount() {
+            return 10;
+        }
+
+        public static class ViewHolder extends RecyclerView.ViewHolder {
+            TextView textView;
+
+            public ViewHolder(@NonNull View itemView) {
+                super(itemView);
+                textView = itemView.findViewById(R.id.textView);
+            }
+        }
     }
 }
