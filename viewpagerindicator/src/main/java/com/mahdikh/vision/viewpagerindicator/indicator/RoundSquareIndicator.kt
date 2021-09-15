d713cc1b3ed091bbc5cc35d9f107e2cf49ce1a27
MahdiDev78 <MahdiDev78@gmail.com>
2021-09-15 07:49:17 +0430

    create SquareIndicator and RoundSquareIndicator

---
+++ viewpagerindicator/src/main/java/com/mahdikh/vision/viewpagerindicator/indicator/RoundSquareIndicator.kt
@@ -0,0 +1,18 @@
+package com.mahdikh.vision.viewpagerindicator.indicator
+
+import android.graphics.Canvas
+import com.mahdikh.vision.viewpagerindicator.info.IndicatorInfo
+import com.mahdikh.vision.viewpagerindicator.util.Paint2
+
+class RoundSquareIndicator : Indicator() {
+    var radius: Float = 5.0F
+
+    override fun onDraw(canvas: Canvas, position: Int, info: IndicatorInfo, paint: Paint2) {
+        canvas.drawRoundRect(
+            info.x, info.y,
+            right(info), bottom(info),
+            radius, radius,
+            paint
+        )
+    }
+}
---
+++ viewpagerindicator/src/main/java/com/mahdikh/vision/viewpagerindicator/indicator/SquareIndicator.kt
@@ -0,0 +1,15 @@
+package com.mahdikh.vision.viewpagerindicator.indicator
+
+import android.graphics.Canvas
+import com.mahdikh.vision.viewpagerindicator.info.IndicatorInfo
+import com.mahdikh.vision.viewpagerindicator.util.Paint2
+
+open class SquareIndicator : Indicator() {
+    override fun onDraw(canvas: Canvas, position: Int, info: IndicatorInfo, paint: Paint2) {
+        canvas.drawRect(
+            info.x, info.y,
+            right(info), bottom(info),
+            paint
+        )
+    }
+}
